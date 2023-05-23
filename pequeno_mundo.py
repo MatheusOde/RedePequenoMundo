071696f6612af6736a54c54fa561ccdbdf1274fd
Matheus <matheusode@gmail.com>
2023-05-23 20:48:11 -0300

    alterada ordem de plot do grafico e adicionada contador de tempo para fazer grafo e busca na rede

--- pequeno_mundo.py
+++ pequeno_mundo.py
@@ -1,9 +1,11 @@
 import networkx as nx
 import random
 import matplotlib.pyplot as plt
+import time
 
 #Gera rede Pequeno Mundo com peso aleatório
 def gerar_rede_pequeno_mundo(n, k, p, peso_min, peso_max):
+    start_time = time.time()
     # Cria um rede de anel regular com k vizinhos para cada vértice
     rede = nx.random_regular_graph(k, n)
 
@@ -21,21 +23,26 @@ def gerar_rede_pequeno_mundo(n, k, p, peso_min, peso_max):
                     w = random.choice(list(rede.nodes()))
                 rede.remove_edge(u, v)
                 rede.add_edge(u, w, weight=random.uniform(peso_min, peso_max))
-                
+                    
+    print("Tempo para gerar a rede: ", time.time() - start_time, "segundos.")
     return rede
 
 #busca em largura
 def busca_em_largura_rede(rede, origem, destino):
+    start_time = time.time()
+    
     try:
         caminho = nx.shortest_path(rede, origem, destino)
         caminho_percurso = [(caminho[i], caminho[i+1]) for i in range(len(caminho)-1)]
-        imprime_rede(rede)
         print("Caminho percorrido da origem ao destino:")
         for u, v in caminho_percurso:
             print(u, "->", v)
+        print("Tempo para executar a busca: ", time.time() - start_time, "segundos.")    
+        imprime_rede(rede)
         return caminho_percurso
     except nx.NetworkXNoPath:
-        print("Não foi encontrado um caminho da origem ao destino.")
+        print("Não foi encontrado um caminho da origem ao destino.")        
+        print("Tempo para executar a busca: ", time.time() - start_time, "segundos.")    
         imprime_rede(rede)
         return None
 
